5bcb068efa5fd2f262e35655a4f12652b23ff870
shinebyul <qufdl8382@gmail.com>
2023-12-25 11:12:45 +0900

    수정11

--- src/app/component/footer.js
+++ src/app/component/footer.js
@@ -1,4 +1,4 @@
-import KartsLogo from '/public/asset/karts_logo_gray.svg'
+import KartsLogo from '/public/asset/kartsLogo2.svg'
 import KartsRightMobile from '/public/asset/kartsRightMobile.svg'
 import KartsRightWeb from '/public/asset/kartsRightWeb.svg'
 
