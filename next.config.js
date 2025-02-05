8934dbc982ff3daabb3a9f52ee6c6360983ceae2
ELEVEN <eleven@ELEVENui-MacBookPro.local>
2025-02-05 15:08:34 +0900

    Contentful 환경 변수 적용 및 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
--- src/app/contentful/contentful.js
+++ src/app/contentful/contentful.js
@@ -1,12 +1,21 @@
 import { createClient } from 'contentful';
 
 export const client = createClient({
-    space: process.env.NEXT_PUBLIC_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-  });
+    space: process.env.NEXT_PUBLIC_SPACE_ID || '',
+    accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN || '',
+});
 
-export const fetchContentful = async(contentType)=> {
-  const res = await client.getEntries({ content_type: contentType });
-  return res.items;
-}
-
+export const fetchContentful = async (contentType) => {
+    if (!process.env.NEXT_PUBLIC_SPACE_ID || !process.env.NEXT_PUBLIC_ACCESS_TOKEN) {
+        console.error('❌ Contentful 환경 변수가 설정되지 않았습니다.');
+        return [];
+    }
+    
+    try {
+        const res = await client.getEntries({ content_type: contentType });
+        return res.items;
+    } catch (error) {
+        console.error('❌ Contentful 데이터 불러오기 실패:', error);
+        return [];
+    }
+};
