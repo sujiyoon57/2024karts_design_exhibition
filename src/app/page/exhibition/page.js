e5a9795f5f91d9ece35e262ec63a194cd42c8025
ELEVEN <eleven@ELEVENui-MacBookPro.local>
2025-02-05 19:07:35 +0900

    Fix: Exhibition 페이지 이미지 렌더링 오류 해결

--- src/app/page/exhibition/page.js
+++ src/app/page/exhibition/page.js
@@ -3,42 +3,46 @@ import Image from "next/image";
 import { fetchContentful } from "@/app/contentful/contentful";
 import Footer from "@/app/component/footer";
 
-export default async function Exhibition(){
-
-    var data = await fetchContentful('portfolio');
+export default async function Exhibition() {
+    var data = await fetchContentful("portfolio");
     const portfolio = data;
-    
-    return(
-      <div className="exhibition">
-        {/* <div className="exhibition-title">{title}</div> */}
-        <div className="exhibition_tab">   
-            <Link href={ `/page/exhibition`} className="active">Projects</Link>
-            <Link href={ `/page/exhibition_list`}>Designers</Link>
-        </div>
-        <div className="exhibition-container">
-            {portfolio && portfolio.map((data,index)=>(
-                <Link href={ `/page/portfolio/${index}`} key={index}>
-                    <div className="exhibition-image-container">
-                        {data.fields.thumbnail.fields.file.url &&
-                            <Image 
-                                src={'https:'+data.fields.thumbnail.fields.file.url} 
-                                alt=".." 
-                                width={0} height={0} 
-                                sizes="100vw"
-                                className={data.fields.thumbnail.fields.file.details.image.height > data.fields.thumbnail.fields.file.details.image.width ? 'isVertical' : ''}
+
+    return (
+        <div className="exhibition">
+            {/* <div className="exhibition-title">{title}</div> */}
+            <div className="exhibition_tab">
+                <Link href={`/page/exhibition`} className="active">Projects</Link>
+                <Link href={`/page/exhibition_list`}>Designers</Link>
+            </div>
+            <div className="exhibition-container">
+                {portfolio && portfolio.map((data, index) => (
+                    <Link href={`/page/portfolio/${index}`} key={index}>
+                        <div className="exhibition-image-container">
+                            {data.fields.thumbnail?.fields?.file?.url && (
+                                <Image
+                                    src={'https:' + data.fields.thumbnail.fields.file.url}
+                                    alt="Project Thumbnail"
+                                    width={0}
+                                    height={0}
+                                    sizes="100vw"
+                                    className={
+                                        (data.fields.thumbnail?.fields?.file?.details?.image?.height || 0) > 
+                                        (data.fields.thumbnail?.fields?.file?.details?.image?.width || 0) 
+                                        ? "isVertical" 
+                                        : ""
+                                    }
                                 />
-                        }
-                        {/* <div className="blackback"></div> */}
-                        
-                    </div>
-                    <div className={`exhibition-info ${data.fields.thumbnailBlack ? 'whiteFont':''}`}>
-                        <div>{data.fields.projectName}</div>
-                        <div className={`exhibition-student-name ${data.fields.thumbnailBlack ? 'whiteFont':''}`}>{data.fields.nameEng}</div>
-                    </div>
-                    
-                </Link>
-            ))}
-        </div> 
-      </div>
-    )
+                            )}
+                        </div>
+                        <div className={`exhibition-info ${data.fields.thumbnailBlack ? "whiteFont" : ""}`}>
+                            <div>{data.fields.projectName}</div>
+                            <div className={`exhibition-student-name ${data.fields.thumbnailBlack ? "whiteFont" : ""}`}>
+                                {data.fields.nameEng}
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
