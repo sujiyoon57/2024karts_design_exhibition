b65451857183529b40dad8c8bc0dd0381681c1c1
t0240117 <wonder666@webidas.com>
2025-02-06 15:18:10 +0900

    footer

--- src/app/component/footer.js
+++ src/app/component/footer.js
@@ -9,11 +9,7 @@ export default function Footer(){
                 <span><KartsLogo/></span>
                 <span>석관동캠퍼스 본부동 (02789)<br />서울특별시 성북구 화랑로32길 146-37</span>
             </div>
-<<<<<<< HEAD
             <p>Copyright(C) Korea National University of Arts Design Dept. <br/>All rights reserved</p>
-=======
-            <p>Copyright(C) Korea National University of Arts Design Department. All rights reserved</p>
->>>>>>> ee3175ec075f008cbd8eb1202d080285f6ca0f75
         </footer>
     )
 }
