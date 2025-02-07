5b11e8f57ee48c7eddfd6cd9041441790bfabd8b
neomb2b-2545 <junpphn@gmail.com>
2025-02-07 16:00:16 +0900

    아카이브 수정중

--- src/app/page/archive/page.js
+++ src/app/page/archive/page.js
@@ -2,43 +2,34 @@ import Link from "next/link";
 import Image from "next/image";
 import { fetchContentful } from "@/app/contentful/contentful"; 
 
-export default async function Notice(){
-    var data = await fetchContentful('notice');
-    const notice = data;
+export default async function ArchiveNew(){
+
+    var data = await fetchContentful('archiveNew');
+    const archiveNew = data;
     
     return( 
         <div className="archive-container"> 
             <ul className="archive_list"> 
+            {archiveNew && archiveNew.map((data, index)=>(     
                 <li>
-                    <a href="/page/archiveIntro"> 
-                        <div className="archive-info">
-                           <Image
+                <Link href={ `/page/archiveIntro/${index}`} key={index}>
+                    <div className="archive-info"> 
+                        <Image
                                 src="/asset/archive2024.png"
                                 alt="Archive 2024"
                                 width={620} 
                                 height={366} 
                                 layout="intrinsic" // ✅ 원본 비율 유지
                                 objectFit="cover" // ✅ 비율을 유지하면서 잘리는 부분 조절
-                            />
-                            <div>2024</div>
-                        </div> 
-                    </a>
+                        />
+                        <div>{data.fields.title}</div>
+                        
+                    </div> 
+                    
+                </Link>
                 </li>
-                <li>
-                    <a href="/page/exhibition"> 
-                        <div className="archive-info">
-                            <Image
-                                src="/asset/archive2023.png"
-                                alt="Archive 2023"
-                                width={620} 
-                                height={366} 
-                                layout="intrinsic" // ✅ 원본 비율 유지
-                                objectFit="cover" // ✅ 비율을 유지하면서 잘리는 부분 조절
-                            />
-                            <div>2023</div>
-                        </div> 
-                    </a>
-                </li>  
+            ))}
+                  
             </ul>
         </div>  
     )
