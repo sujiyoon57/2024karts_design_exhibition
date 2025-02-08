6f7756a0d373b76e47fe79eda8a22b6e0b70dbfa
ELEVEN <eleven@ELEVENui-MacBookPro.local>
2025-02-08 16:35:36 +0900

    Update department description font weight

--- src/app/page/professor/page.js
+++ src/app/page/professor/page.js
@@ -1,13 +1,11 @@
 "use client"
 
 import { fetchContentful } from "@/app/contentful/contentful";
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Footer from '@/app/component/footer';
 import KartsLinkLogo from "/public/asset/kartsLinkLogo.svg";
- 
 
 export default function Karts(){
 
@@ -16,10 +14,8 @@ export default function Karts(){
     useEffect(() => {
         const getContentful=async()=>{
           try{
-
             var data = await fetchContentful('professor');
             setProfessor(data);
-
           }catch (error) {
             console.error("Error fetching data:", error);
           }
@@ -27,20 +23,17 @@ export default function Karts(){
         getContentful();
       }, []);
 
-    
-
     return(
-        <div className="container1 professor">
+        <div className="container1 professor professor-page"> {/* ✅ 특정 페이지 클래스 추가 */}
             <div className="container2">
                 <div className="exhibition_tab">   
                     <Link href={ `/page/karts`}>학과 및 교수 소개</Link>
                     <Link href={ `/page/professor`} className="active">겸임 · 강사</Link>
                 </div> 
                  
-                 
                 <div className="professor_list">
                      {professor && professor.map((data, index)=>(
-                        <div className="prof_con">
+                        <div className="prof_con" key={index}>
                             <div className="prof_name">{data.fields.name}</div>
                             <div className="prof_info">
                                 <p><span>전공분야</span> <em>{data.fields.major}</em></p>
@@ -49,9 +42,7 @@ export default function Karts(){
                             </div> 
                         </div>      
                     ))}
-
                 </div> 
-              
             </div> 
         </div>
     );
