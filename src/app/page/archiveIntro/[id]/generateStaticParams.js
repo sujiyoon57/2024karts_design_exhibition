a3504fa599067a46e539804148f00b23df42e34a
ELEVEN <eleven@ELEVENui-MacBookPro.local>
2025-02-09 14:50:40 +0900

    Update department description font weight

--- src/app/page/archiveIntro/[id]/generateStaticParams.js
+++ src/app/page/archiveIntro/[id]/generateStaticParams.js
@@ -5,7 +5,7 @@ export async function generateStaticParams() {
 
     if (!data || !Array.isArray(data)) return [];
 
-    return data.map((_, index) => ({
-        id: index.toString(),
+    return data.map((item) => ({
+        id: item.sys.id, // ✅ Contentful의 sys.id를 사용하여 고유한 경로 생성
     }));
 }
