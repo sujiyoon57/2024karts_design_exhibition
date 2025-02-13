3fccce73ee56e8df7e0507e81c5483dc04755dd8
sujiyoun57 <jisoo5257@gmail.com>
2025-02-13 15:35:41 +0900

    메인->공지사항 수정정

--- src/app/page/mainPage/noticePage.js
+++ src/app/page/mainPage/noticePage.js
@@ -33,6 +33,7 @@ export default function MainPage() {
     };
   }, []);
 
+<<<<<<< HEAD
     return(
         <div className="main-notice"> 
             <h3>공지사항</h3>
@@ -69,3 +70,39 @@ export default function MainPage() {
         </div>
     )
 }
+=======
+  return (
+    <div className="main-notice">
+      <h3>공지사항</h3>
+      <ul className="notice_list">
+        {notices.length > 0 ? (
+          notices.map((data, index) => (
+            <li key={index}>
+              <a href={`/page/notice_view/${index}`}>
+                <div className="notice-info">
+                  <div
+                    className={
+                      data.fields.part2.includes("학과")
+                        ? "type01"
+                        : data.fields.part2.includes("행사")
+                        ? "type02"
+                        : data.fields.part2.includes("채용")
+                        ? "type03"
+                        : ""
+                    }
+                  >{data.fields.part2}</div>
+                  <div>{data.fields.title}</div>
+                  <div>{data.sys.createdAt.substr(0, 10)}</div>
+                </div>
+              </a>
+            </li>
+          ))
+        ) : (
+          <li>공지사항이 없습니다.</li>
+        )}
+      </ul>
+      <a href="/page/notice" className="more">전체 보기</a>
+    </div>
+  );
+}
+>>>>>>> ec04736 (메인->공지사항 수정정)
