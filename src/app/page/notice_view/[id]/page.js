eb67f78dde1fbdcc293c284be614cebcade1ced2
sujiyoun57 <jisoo5257@gmail.com>
2025-02-14 08:56:52 +0900

    공지사항 상세보기 줄바꿈 처리리

--- src/app/page/notice_view/[id]/page.js
+++ src/app/page/notice_view/[id]/page.js
@@ -1,48 +1,73 @@
 "use client"
+
 import Link from "next/link"
-import Image from "next/image"
+import { useState, useEffect } from "react"
 import { fetchContentful } from "@/app/contentful/contentful"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import InstaLogoIcon from "/public/asset/instaLogo.svg"
-import WebLogoIcon from "/public/asset/webLogo.svg"
-import VimeoLogoIcon from "/public/asset/vimeoLogo.svg"
-import Footer from "@/app/component/footer"
-import ScrollUp from "@/app/component/scrollUp"
-import ReactPlayer from "react-player"
-import { useSearchParams } from 'next/navigation'
+import { BLOCKS, INLINES, MARKS } from "@contentful/rich-text-types";
 
+export default function Notice({ params }) {
+    const id = parseInt(params.id);
+    const [notice, setNotice] = useState(null);
+    const [sys, setSys] = useState(null);
 
-export default async function Notice(props){
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchContentful('notice');
+                if (data[id]) {
+                    setNotice(data[id].fields);
+                    setSys(data[id].sys);
+                }
+            } catch (error) {
+                console.error("Error fetching notice data:", error);
+            }
+        };
+        fetchData();
+    }, [id]);
 
-    const id = parseInt(props.params.id);
-    const data = await fetchContentful('notice');
-    const notice = data[id].fields;
-    const sys = data[id].sys;
-    const len  = data.length > 0 && data.length; 
-    //const timeFormat = searchParams.get("timeFormat")
+    if (!notice) return <div>Loading...</div>;
 
-    //console.log(notice);
+    const options = {
+      renderText: (text) => {
+        return text.split("\n").map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+        ));
+      },
+    };
 
-    return(
-
+    return (
         <div className="notiview-container">
             <div className="notiview-top">
                 <div className="notiview_tit">{notice.title}</div>
-                <div className="notiview-info"> 
+                <div className="notiview-info">
                     <div><span>작성자</span> <em>{notice.writer}</em></div>
-                    <div><span>작성일</span> <em>{(sys.createdAt).substr(0, 10)}</em></div>
-                    {notice.file !== undefined && notice.file.fields.file.url !== '' ? (
-                        <div><span>첨부파일</span> <em><Link href={'https:'+notice.file.fields.file.url}>{notice.file.fields.file.fileName}({notice.file.fields.file.details.size}byte)</Link></em></div> 
-                    ) : null}
+                    <div><span>작성일</span> <em>{sys?.createdAt?.substr(0, 10)}</em></div>
+                    {notice.file?.fields?.file?.url && (
+                        <div>
+                            <span>첨부파일</span>
+                            <em>
+                                <Link href={`https:${notice.file.fields.file.url}`} target="_blank">
+                                    {notice.file.fields.file.fileName} ({notice.file.fields.file.details.size} byte)
+                                </Link>
+                            </em>
+                        </div>
+                    )}
                 </div>
             </div>
-            <div className="notiview-cont"> 
-                {notice.contents && notice.contents.content.map((data)=>(
-                    <p key={data.id} className="about-flex-item">
-                        {documentToReactComponents(data)}</p>
-                ))}
-            </div> 
-            <div className="notiview-back"><Link href='/page/notice'>Back to List</Link></div> 
+            <div className="notiview-cont">
+                {notice.contents && (
+                <div className="notiview-cont">
+                    {documentToReactComponents(notice.contents, options)}
+                </div>
+                )}
+            </div>
+            <div className="notiview-back">
+                <Link href='/page/notice'>Back to List</Link>
+            </div>
         </div>
     );
 }
