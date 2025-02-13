c6835b6823844cd3cd846dd7ca047a0c0f2d93ab
sujiyoun57 <jisoo5257@gmail.com>
2025-02-14 08:52:32 +0900

    메인페이지 공지사항 수정정

--- src/app/page/mainPage/noticePage.js
+++ src/app/page/mainPage/noticePage.js
@@ -1,112 +1,69 @@
 "use client"
-import Image from "next/image"
-import { fetchContentful } from "@/app/contentful/contentful"
-import ScrollDown from "@/app/component/scrollDown"
-import About from "@/app/component/about";
-import { useSearchParams } from "next/navigation";
-import { useState,useEffect } from "react";
 
-export default function MainPage(){
-    const [title, setTitle] = useState();
-    const [title2, setTitle2] = useState();
-    const [title3, setTitle3] = useState();
-    const [title4, setTitle4] = useState();
+import { fetchContentful } from "@/app/contentful/contentful";
+import { useState, useEffect } from "react";
 
-    const [date, setDate] = useState();
-    const [date2, setDate2] = useState();
-    const [date3, setDate3] = useState();
-    const [date4, setDate4] = useState();
+export default function MainPage() {
+  const [scrollup, setScrollup] = useState(false);
+  const [notices, setNotices] = useState([]);
 
-    useEffect(() => {
-        const getContentful=async()=>{
-            try {
-                var data = await fetchContentful('notice');
-                setTitle(data[0].fields.title);
-                setDate((data[0].sys.createdAt).substr(0, 10));
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
+  useEffect(() => {
+    const getContentful = async () => {
+      try {
+        var data = await fetchContentful("notice");
 
-            try {
-                var data = await fetchContentful('notice2');
-                setTitle2(data[0].fields.title);
-                setDate2((data[0].sys.createdAt).substr(0, 10));
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
+        // 최신 등록순 정렬 후 4개만 선택
+        const sortedData = data
+          .sort((a, b) => new Date(b.sys.createdAt) - new Date(a.sys.createdAt))
+          .slice(0, 4);
 
-            try {
-                var data = await fetchContentful('notice3');
-                setTitle3(data[0].fields.title);
-                setDate3((data[0].sys.createdAt).substr(0, 10));
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
+        setNotices(sortedData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-            try {
-                var data = await fetchContentful('notice4'); 
-                setTitle4(data[0].fields.title);
-                setDate4((data[0].sys.createdAt).substr(0, 10));
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
+    getContentful();
 
-        getContentful();
-    }, []);
+    const aboutTimeout = setTimeout(() => {
+      setScrollup(true);
+    }, 5000);
+    return () => {
+      clearTimeout(aboutTimeout);
+    };
+  }, []);
 
     return(
         <div className="main-notice"> 
             <h3>공지사항</h3>
             <ul className="notice_list">
-               {title && (
-                <li>
-                  <a href={ `/page/notice_view/0`}>
-                    <div className="notice-info">
-                      <div className="type01">학과</div>  {/* 학과 (핑크) */}
-                      <div>{title}</div> 
-                      <div>{date}</div>
-                    </div>
-                  </a>
-                </li>
-                )}
-
-               {title2 && (
-                <li>
-                  <a href={ `/page/notice2_view/0`}>
-                    <div className="notice-info">
-                      <div className="type02">행사</div>  {/* 행사 (그린) */}
-                      <div>{title2}</div> 
-                      <div>{date2}</div>
-                    </div>
-                  </a>
-                </li>
-                )}
-
-                {title3 && (
-                <li>
-                  <a href={ `/page/notice3_view/0`}>
-                    <div className="notice-info">
-                      <div className="type03">채용</div>  {/* 채용 (블루) */}
-                      <div>{title3}</div> 
-                      <div>{date3}</div>
-                    </div>
-                  </a>
-                </li>
-                )}
-
-                {title4 && (
-                <li>
-                  <a href={ `/page/notice4_view/0`}>
-                    <div className="notice-info">
-                      <div className="type04">기타</div>  {/* 기타 (오렌지) */}
-                      <div>{title4}</div> 
-                      <div>{date4}</div>
-                    </div>
-                  </a>
-                </li> 
-                 )}      
-
+              {notices.length > 0 ? (
+                notices.map((data, index) => (
+                  <li key={index}>
+                    <a href={`/page/notice_view/${index}`}>
+                      <div className="notice-info">
+                      <div
+                        className={
+                          data.fields.part2.includes("학과")
+                            ? "type01"
+                            : data.fields.part2.includes("행사")
+                            ? "type02"
+                            : data.fields.part2.includes("채용")
+                            ? "type03"
+                            : data.fields.part2.includes("기타")
+                            ? "type04"
+                            : ""
+                        }
+                      >{data.fields.part2}</div>
+                        <div>{data.fields.title}</div>
+                        <div>{data.sys.createdAt.substr(0, 10)}</div>
+                      </div>
+                    </a>
+                  </li>
+                ))
+              ) : (
+                <li>공지사항이 없습니다.</li>
+              )}
             </ul>
             <a href="/page/notice" className="more">전체 보기</a> 
         </div>
