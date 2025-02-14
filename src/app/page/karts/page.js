52dd40dbe6335aa0fccba5ef3d2dbfafca356826
ELEVEN <eleven@ELEVENui-MacBookPro.local>
2025-02-14 19:55:51 +0900

    모바일 교수 이미지 크기 고정 및 정렬 수정

--- src/app/page/karts/page.js
+++ src/app/page/karts/page.js
@@ -40,65 +40,66 @@ export default function Karts() {
 
     return (
         <>
-        <Header menuOn={menuOn} setMenuOn={setMenuOn} />
-        <div className="container1">
-            <div className="container2">
-                <div className="exhibition_tab">
-                    <Link href={`/page/karts`} className="active">학과 및 교수 소개</Link>
-                    <Link href={`/page/professor`}>겸임 · 강사</Link>
-                </div>
-                <div className="kart_tit">학과소개</div>
-                
-                {aboutButton && about && about.map((data) => (
-                    <div key={data.id} className="flex-box">
-                        {data.fields.Images && data.fields.Images.map((img) => (
-                            <div key={img.id} className="about-flex-item">
-                                <Image
-                                    src={'https:' + img.fields.file.url}
-                                    alt="이미지"
-                                    width={432}  // 적절한 width로 설정
-                                    height={240} // 적절한 height로 설정
-                                    sizes="100vw"
-                                    layout="responsive"
-                                />
-                            </div>
-                        ))}
-                        {data.fields.caption && (
-                            <div className="about-flex-item">
-                                {documentToReactComponents(data.fields.caption, options)}
-                            </div>
-                        )}
+            <Header menuOn={menuOn} setMenuOn={setMenuOn} />
+            <div className="container1">
+                <div className="container2">
+                    <div className="exhibition_tab">
+                        <Link href={`/page/karts`} className="active">학과 및 교수 소개</Link>
+                        <Link href={`/page/professor`}>겸임 · 강사</Link>
                     </div>
-                ))}
+                    <div className="kart_tit">학과소개</div>
 
-                <div className="kart_tit">교수진</div>
-                <div className="flex-box">
-                    {faculty && faculty.map((data, index) => (
-                        <div key={data.id || index} className="faculty-flex-item">
-                            <Image
-                                src={'https:' + data.fields.image.fields.file.url}
-                                alt="교수 이미지"
-                                width={150}  // 적절한 width로 설정
-                                height={150} // 적절한 height로 설정
-                                sizes="100vw"
-                                layout="intrinsic"
-                            />
-                            <div className="faculty-info">
-                                {data.fields.name && <div>{data.fields.name}</div>}
-                                <div>{data.fields.nameEn}</div>
-                                <div>{data.fields.major}</div>
-                                <div></div>
-                                <div
-                                    dangerouslySetInnerHTML={{
-                                        __html: (data.fields.education3 || "").replace(/\n/g, "<br />"),
-                                    }}
-                                />
-                            </div>
+                    {aboutButton && about && about.map((data) => (
+                        <div key={data.id} className="flex-box">
+                            {data.fields.Images && data.fields.Images.map((img) => (
+                                <div key={img.id} className="about-flex-item">
+                                    <Image
+                                        src={'https:' + img.fields.file.url}
+                                        alt="이미지"
+                                        width={432}  // 적절한 width로 설정
+                                        height={240} // 적절한 height로 설정
+                                        sizes="100vw"
+                                        layout="responsive"
+                                    />
+                                </div>
+                            ))}
+                            {data.fields.caption && (
+                                <div className="about-flex-item">
+                                    {documentToReactComponents(data.fields.caption, options)}
+                                </div>
+                            )}
                         </div>
                     ))}
+
+                    <div className="kart_tit">교수진</div>
+                    <div className="flex-box">
+                        {faculty && faculty.map((data, index) => (
+                            <div key={data.id || index} className="faculty-flex-item">
+                                <Image
+                                    src={'https:' + data.fields.image.fields.file.url}
+                                    alt="교수 이미지"
+                                    width={150}  // 적절한 width로 설정
+                                    height={150} // 적절한 height로 설정
+                                    sizes="100vw"
+                                    layout="intrinsic"
+                                />
+                                <div className="faculty-info">
+                                    {data.fields.name && <div>{data.fields.name}</div>}
+                                    <div>{data.fields.nameEn}</div>
+                                    <div>{data.fields.major}</div>
+                                    <div></div>
+                                    <div className="education-text" // ✅ 새로운 class 추가
+                                        dangerouslySetInnerHTML={{
+                                            __html: (data.fields.education3 || "").replace(/\n/g, "<br />"),
+                                        }}
+                                    />
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
