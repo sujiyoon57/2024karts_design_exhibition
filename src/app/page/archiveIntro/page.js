0142b54040414a6ff09a44bb7e6a8cc47d341300
shinebyul <qufdl8382@gmail.com>
2025-12-15 21:12:48 +0900

    [perf] archiveIntro/page.js CSR를 SSR로 변경 (사용되지 않는 페이지)

--- src/app/page/archiveIntro/page.js
+++ src/app/page/archiveIntro/page.js
@@ -1,49 +1,17 @@
-"use client";
-
 import { fetchContentful } from "@/app/contentful/contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { useState, useEffect } from "react";
 import Link from "next/link";
-import Header from "@/app/component/header";
 
-export default function ArchiveIntroPage({ params }) {
+export default async function ArchiveIntroPage({ params }) {
     const { slug } = params; // ✅ URL에서 slug 값 가져오기
-    const [archiveNew, setArchiveNew] = useState(null);
-    const [activeTab, setActiveTab] = useState("info");
-    const [menuOn, setMenuOn] = useState(false);
-    const [loading, setLoading] = useState(true);
 
-    // ✅ 데이터를 가져오는 useEffect
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchContentful("archiveNew"); 
-                
-                if (data) {
-                    // ✅ slug 값으로 데이터 찾기
-                    const matchedData = data.find(item => item.fields.slug === slug);
-                    
-                    if (matchedData) {
-                        setArchiveNew(matchedData.fields);
-                    } else {
-                        console.error("Error: 해당 slug를 가진 데이터가 없습니다.");
-                    }
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, [slug]);
+    const data = await fetchContentful("archiveNew");
+    const archiveNew = data.find(item => item.fields.slug === slug).fields;
 
-    if (loading) return <p>Loading...</p>; // ✅ 로딩 상태 추가
     if (!archiveNew) return <p>데이터를 불러올 수 없습니다.</p>; // ✅ 데이터가 없을 때 처리
 
     return (
         <div className="archive-container">
-            <Header menuOn={menuOn} setMenuOn={setMenuOn} />
             <div className="archive_intro archive_intro_web">
                 <div className="backtolist">
                     <Link href="/page/archive">⟵<span>Back to Lists</span></Link>
