407c44080fc0abdf62408ab82c85f70fe50acbd8
shinebyul <qufdl8382@gmail.com>
2025-12-15 21:23:30 +0900

    [perf] 데이터는 MainPage에서 넘겨줘서 CSR에서 데이터 fetch가 안되도록 수정 + poster, posterm을 poster로만 통일

--- src/app/page/home/MainSlider.js
+++ src/app/page/home/MainSlider.js
@@ -1,10 +1,4 @@
 "use client"
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Link from "next/link";
-import Image from "next/image";
-import { fetchContentful } from '../../contentful/contentful';
-
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'; // 주요 Swiper CSS 파일을 가져옵니다.
@@ -15,27 +9,12 @@ import { Navigation, Scrollbar, Autoplay, Pagination } from 'swiper/modules';
 import SwiperCore from 'swiper';
 SwiperCore.use([Autoplay, Pagination, Navigation]);
 
-
-export default function MainPage() {
-
-    const [poster, setPoster] = useState();
-    const [posterm, setPosterm] = useState();
-
-    useEffect(() => {
-        const getContentful = async () => {
-            try {
-
-                var data = await fetchContentful('poster');
-                setPoster(data);
-                setPosterm(data);
-                //console.log(data);
-
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
-        getContentful();
-    }, []);
+export default function MainSlider({ poster }) {
+    /*
+    * 2025.12
+    * 기존에 poster과 posterm를 각각 만들어 같은 data를 넣음
+    * poster는 pc버전에, posterm은 모바일 버전으로 사용됨
+    * -> posterm은 없애고 poster로 통일시킴*/
 
     return (
         <div className="main_visual">
@@ -64,7 +43,7 @@ export default function MainPage() {
                     pagination={true}
                     className="mySwiper"
                 >
-                    {posterm && posterm.map((data) => (
+                    {poster && poster.map((data) => (
                         <SwiperSlide key={data.sys.id || Math.random()}>  
                             <a href={data.fields.link || "https://www.kartsdesign.co.kr/page/archiveIntro/0"} target="_blank" rel="noopener noreferrer">
                                 <img src={'https:' + data.fields.posterImageMobile.fields.file.url} alt={`poster-mobile-${data.sys.id}`} />
